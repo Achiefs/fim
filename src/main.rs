c4e32a6d09a6ff02e91d7fe501fc7f82b518493c
José Fernández <pylott@gmail.com>
2021-01-12 13:09:56 +0100

    Added Checksum support

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@
 use std::fs::{File, OpenOptions};
 use std::io::Read;
 use std::io::Write;
+use std::io::{Error, ErrorKind};
+use std::result::Result;
 // To parse files in yaml format
 use yaml_rust::yaml::{Yaml, YamlLoader};
 // To get file checksums
-//use crypto::digest::Digest;
-//use crypto::sha3::Sha3;
+use crypto::digest::Digest;
+use crypto::sha3::Sha3;
 // To get file system changes
 use notify::{RecommendedWatcher, Watcher, RecursiveMode, RawEvent};
 use notify::op::Op;
@@ -28,12 +30,16 @@ fn read_config(file: &str) -> Vec<Yaml> {
 
 
 // To calculate file content hash in sha512 format (SHA3 implementation)
-/*fn get_checksum(file: &str) -> String {
+fn get_checksum(file: &str) -> Result<String, Error> {
     let mut hasher = Sha3::sha3_512();
-    let data = std::fs::read_to_string(file).unwrap();
-    hasher.input_str(&data);
-    hasher.result_str()
-}*/
+    match std::fs::read_to_string(file) {
+        Ok(data) => {
+            hasher.input_str(&data);
+            return Ok(hasher.result_str())
+        },
+        Err(e) => return Err(e),
+    }
+}
 
 // Function to write the received events to file
 fn log_event(file: &str, event: RawEvent){
@@ -46,13 +52,38 @@ fn log_event(file: &str, event: RawEvent){
 
     let path = event.path.expect("Event path");
     match event.op.unwrap() {
-        Op::CREATE => writeln!(log, "File '{}' created", path.to_str().unwrap()).expect("Error writing event"),
-        Op::WRITE => writeln!(log, "File '{}' written", path.to_str().unwrap()).expect("Error writing event"),
-        Op::REMOVE => writeln!(log, "File '{}' removed", path.to_str().unwrap()).expect("Error writing event"),
-        Op::RENAME => writeln!(log, "File '{}' renamed", path.to_str().unwrap()).expect("Error writing event"),
-        Op::CHMOD => writeln!(log, "File '{}' modified", path.to_str().unwrap()).expect("Error writing event"),
-        Op::CLOSE_WRITE => writeln!(log, "File '{}' closed", path.to_str().unwrap()).expect("Error writing event"),
-        Op::RESCAN => writeln!(log, "Directory '{}' need to be rescaned", path.to_str().unwrap()).expect("Error writing event"),
+        Op::CREATE => {
+            let checksum = get_checksum(path.to_str().unwrap()).unwrap();
+            writeln!(log, "File '{}' created, checksum {}",
+                path.to_str().unwrap(), checksum).expect("Error writing event");
+        }
+        Op::WRITE => {
+            let checksum = get_checksum(path.to_str().unwrap()).unwrap();
+            writeln!(log, "File '{}' written, new checksum {}",
+                path.to_str().unwrap(), checksum).expect("Error writing event");
+        }
+        Op::REMOVE => writeln!(log, "File '{}' removed",
+            path.to_str().unwrap()).expect("Error writing event"),
+        Op::RENAME => {
+            let checksum = match get_checksum(path.to_str().unwrap()) {
+                Ok(data) => data,
+                Err(e) => {
+                    match e.kind() {
+                        ErrorKind::NotFound => println!("File Not found error ignoring..."),
+                        _ => panic!("Not handled error on get_checksum function."),
+                    };
+                    String::from("IGNORED")
+                }
+            };
+            writeln!(log, "File '{}' renamed, checksum {}",
+                path.to_str().unwrap(), checksum).expect("Error writing event");
+        }
+        Op::CHMOD => writeln!(log, "File '{}' permissions modified",
+            path.to_str().unwrap()).expect("Error writing event"),
+        Op::CLOSE_WRITE => writeln!(log, "File '{}' closed",
+            path.to_str().unwrap()).expect("Error writing event"),
+        Op::RESCAN => writeln!(log, "Directory '{}' need to be rescaned",
+            path.to_str().unwrap()).expect("Error writing event"),
         _ => error!("Event Op not Handled"),
     }
 }
@@ -60,7 +91,7 @@ fn log_event(file: &str, event: RawEvent){
 
 // Main function where the magic happens
 fn main() {
-    let config_path = "src/config.yml";
+    let config_path = "config.yml";
     let config = read_config(config_path);
     let paths = &config[0]["monitor"];
     //let delay:u64 = config[0]["watcher"]["delay"].as_i64().unwrap().try_into().unwrap();
