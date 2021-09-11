6b7c498953f28e26a145fa3d43d063c2cce048b4
José Fernández <pylott@gmail.com>
2021-09-11 14:13:56 +0200

    Work with Paths

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::sync::mpsc::channel;
 // To log the program process
 use log::*;
 use simplelog::{WriteLogger, Config};
+// To manage paths
+use std::path::Path;
 
 // To load configuration functions
 mod config;
@@ -14,16 +16,17 @@ mod events;
 
 // Main function where the magic happens
 fn main() {
+    println!("Reading config...");
+
     let config_path = "config.yml";
     let config = config::read_config(config_path);
     let paths = &config[0]["monitor"];
-    //let delay:u64 = config[0]["watcher"]["delay"].as_i64().unwrap().try_into().unwrap();
     let log_file = &config[0]["log"]["output"]["file"].as_str().unwrap();
     let log_level = &config[0]["log"]["output"]["level"].as_str().unwrap();
     let events_file = &config[0]["log"]["events"]["file"].as_str().unwrap();
     let events_format = &config[0]["log"]["events"]["format"].as_str().unwrap();
+    let ignores = &config[0]["ignore"];
     
-    println!("Reading config...");
     println!("Config file: {}", config_path);
     println!("Log file: {}", log_file);
     println!("Events file: {}", events_file);
@@ -55,6 +58,20 @@ fn main() {
         match rx.recv() {
             Ok(event) => {
                 debug!("Event registered: {:?}", event);
+                for ignore in ignores.as_vec().unwrap() {
+                    let ignore_path = Path::new(ignore.as_str().unwrap());
+                    let ignore_parent_path = ignore_path.parent().unwrap().to_str().unwrap();
+                    let event_path = Path::new(event.path.as_ref().unwrap().to_str().unwrap());
+                    let event_parent_path = event_path.parent().unwrap().to_str().unwrap();
+                    println!("Event path: {}", event_path.to_str().unwrap());
+                    println!("Ignore path: {}", ignore_path.to_str().unwrap());
+                    println!("Event parent path: {}", event_parent_path);
+                    println!("Ignore parent path: {}", ignore_parent_path);
+                    //match parent {
+                    //    event.path => println!('EQUALS');
+                    //}
+                    //println!("Character: {}",  );
+                }
                 events::log_event(events_file, event, events_format)
             },
             Err(e) => error!("Watch error: {:?}", e),
