23d78c9e8a30d12eaac598eb9e0cbe629405f6df
José Fernández <pylott@gmail.com>
2022-02-12 13:39:30 +0100

    Added unit test to config and hash files

--- src/event/hash.rs
+++ src/event/hash.rs
@@ -19,3 +19,43 @@ pub fn get_checksum(file: &str) -> Result<String, Error> {
         Err(e) => return Err(e),
     }
 }
+
+// ----------------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::fs::File;
+    use std::io::prelude::*;
+
+    fn create_test_file(filename: &str) {
+        File::create(filename).unwrap().write_all(b"This is a test!").unwrap();
+    }
+
+    fn remove_test_file(filename: &str) {
+        fs::remove_file(filename).unwrap()
+    }
+
+    #[test]
+    fn test_get_checksum_file() {
+        let filename = "test_get_checksum_file";
+        create_test_file(filename);
+        assert_eq!(get_checksum(filename).unwrap(), "46512636eeeb22dee0d60f3aba6473b1fb3258dc0c9ed6fbdbf26bed06df796bc70d4c1f6d50ca977b45f35b494e4bd9fb34e55a1576d6d9a3b5e1ab059953ee");
+        remove_test_file(filename);
+    }
+
+    #[test]
+    #[should_panic(expected = "NotFound")]
+    fn test_get_checksum_panic() {
+        assert_ne!(get_checksum("not_exists").unwrap(), "This is a test");
+    }
+
+    #[test]
+    fn test_get_checksum_bad() {
+        let filename = "test_get_checksum_bad";
+        create_test_file(filename);
+        assert_ne!(get_checksum(filename).unwrap(), "This is a test");
+        remove_test_file(filename);
+    }
+}
