01d3373ce56f4ce4c2ce8aefe03ef4734d60a0d4
José Fernández <pylott@gmail.com>
2022-10-01 23:50:55 +0200

    Fixed rotation special ignore case

--- src/logreader.rs
+++ src/logreader.rs
@@ -26,7 +26,7 @@ type SHashMap = HashMap<String, String>;
 // ----------------------------------------------------------------------------
 
 // Read file to extract last data until the Audit ID changes
-pub fn read_log(file: String, config: config::Config, position: u64) -> (Event, u64) {
+pub fn read_log(file: String, config: config::Config, position: u64, itx: u64) -> (Event, u64) {
     let mut event: Event = Event::new();
     let mut current_position = position;
     let end_position = utils::get_file_end(&file);
@@ -94,7 +94,9 @@ pub fn read_log(file: String, config: config::Config, position: u64) -> (Event, 
             line["type"] == "PATH" ||
             line["type"] == "PROCTITLE"
         }) {
-            current_position = position;
+            if itx < 3{
+                current_position = position;
+            }
         }
     }
     (event, current_position)
