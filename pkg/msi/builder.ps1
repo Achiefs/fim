94ad4b5b5edd1527319780674e52214ead3ce27e
José Fernández <pylott@gmail.com>
2022-12-02 00:10:07 +0100

    Applied fixes to MSI installer package

--- pkg/msi/builder.ps1
+++ pkg/msi/builder.ps1
@@ -2,5 +2,5 @@ $version = (gc ..\..\Cargo.toml | findstr "version" | select -First 1).split(" "
 
 Invoke-Expression "& `"C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe`" .\fim.wxs -o .\fim.wixobj"
 
-Invoke-Expression "& `"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe`" -ext WixUIExtension .\fim.wixobj -o fim-$version-1.msi"
+Invoke-Expression "& `"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe`" -ext WixUIExtension .\fim.wixobj -o fim-$version-1-x64.msi"
 
