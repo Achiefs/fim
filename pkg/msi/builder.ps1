03d51159146099daf399c7b712bb5a99570bdb42
José Fernández <pylott@gmail.com>
2022-12-11 18:57:08 +0100

    Added compilation in MSI builder, added MSI to LFS ignore

--- pkg/msi/builder.ps1
+++ pkg/msi/builder.ps1
@@ -1,5 +1,12 @@
 $version = (gc ..\..\Cargo.toml | findstr "version" | select -First 1).split(" ")[2].trim("`"", " ")
 
+cd ..\..\
+cargo build --release
+cd pkg\msi
+
+cp ..\..\target\release\fim.exe .\
+cp ..\..\config\windows\config.yml .\
+
 Invoke-Expression "& `"C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe`" .\fim.wxs -o .\fim.wixobj"
 
 Invoke-Expression "& `"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe`" -ext WixUIExtension .\fim.wixobj -o fim-$version-1-x64.msi"
