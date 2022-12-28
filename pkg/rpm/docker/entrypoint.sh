68b497c9455459586d4a31eafa842600814ee95f
José Fernández <pylott@gmail.com>
2022-12-28 02:03:21 +0100

    Added building dockers to push at docker hub

---
+++ pkg/rpm/docker/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ref=$1
+
+mkdir -p /tmp/output
+mkdir -p /tmp/repo
+cd /tmp/repo
+git clone https://github.com/Achiefs/fim.git -b ${ref} --depth=1
+cd fim/pkg/rpm
+./builder.sh
+cp fim-*.rpm /tmp/output/
+chown 1000:1000 /tmp/output/*
